bb44038d10aef11db7e7dccd87b10b6623dc45b6
DavideTessari <davtex@gmail.com>
2024-03-13 15:38:14 +0100

    ex-while complete

--- Ciclo-while/js/script.js
+++ Ciclo-while/js/script.js
@@ -15,3 +15,25 @@
 // - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 // - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
 
+// Lista Array
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+// Stampo ogni elemento nell'array 
+const userList = document.querySelector('#user-message');
+
+let i = 0;
+while(i < list.length) {
+    const listItem = list[i];
+    i++;
+
+    const arrayList = `<div>${listItem}</div>`;
+
+    userList.innerHTML += arrayList;
+}
